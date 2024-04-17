d1b50af72842e52c16db7169c3cd7e4512d9f336
AchmadPutraA12 <ahmad.arifki11@gmail.com>
2024-04-17 17:57:08 +0700

    tugas

--- Model.py
+++ Model.py
@@ -38,24 +38,27 @@ class Model_CNN_IR:
         #create CNN model
         model = tf.keras.Sequential([
             tf.keras.layers.Input(shape=input_shape),
-            tf.keras.layers.Conv2D(16, kernel_size=(3, 3),activation='relu'),
+            tf.keras.layers.Conv2D(32, kernel_size=(3, 3),activation='relu'),
             tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
             tf.keras.layers.Conv2D(32, kernel_size=(3, 3),activation='relu'),
             tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
-            tf.keras.layers.Conv2D(64, kernel_size=(3, 3), activation='relu'),
+            tf.keras.layers.Conv2D(32, kernel_size=(3, 3), activation='relu'),
             tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
-            # tf.keras.layers.Conv2D(8, kernel_size=(3, 3), activation='relu'),
-            # tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+            tf.keras.layers.Conv2D(32, kernel_size=(3, 3), activation='relu'),
+            tf.keras.layers.MaxPooling2D(pool_size=(3, 3)),
+            tf.keras.layers.Conv2D(18, kernel_size=(2, 2), activation='relu'),
+            tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
             # tf.keras.layers.Conv2D(5, kernel_size=(3, 3), activation='relu'),
             tf.keras.layers.Flatten(),
-            # tf.keras.layers.Dense(100, activation='relu'),
+            tf.keras.layers.Dense(100, activation='relu'),
+            tf.keras.layers.Dense(100, activation='relu'),
             tf.keras.layers.Dense(output_dim, activation='softmax')
         ])
         model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
         self.model = model
 
     def train_model(self):
-        self.history = self.model.fit(self.trainGenerator, epochs=20, verbose=2)
+        self.history = self.model.fit(self.trainGenerator, epochs=40, verbose=4)
 
     def model_summary(self):
         self.model.summary()
--- main.py
+++ main.py
@@ -1,15 +1,28 @@
+import numpy as np
+import tensorflow as tf
 from sklearn.metrics import accuracy_score
-
 from Model import Model_CNN_IR
 from Dataset import load_storage
 
+# Fungsi untuk menyerialisasi contoh ke format TFRecord
+def serialize_example(feature, label):
+    # Membuat dictionary untuk feature dan label
+    feature_dict = {
+        'feature': tf.train.Feature(float_list=tf.train.FloatList(value=feature.flatten())),
+        'label': tf.train.Feature(int64_list=tf.train.Int64List(value=[label])),
+    }
+    # Membuat example
+    example = tf.train.Example(features=tf.train.Features(feature=feature_dict))
+    return example.SerializeToString()
+
+# Main script
 if __name__ == '__main__':
-    #objek dataset
+    # Objek dataset
     load = load_storage()
     print("shape x_train", load.x_train.shape)
     print("shape y_train", load.y_train.shape)
 
-    #model ML
+    # Model ML
     ML = Model_CNN_IR(load.x_train, load.y_train, load.x_test, load.y_test)
     ML.datagenerate()
     ML.create_architecture()
@@ -17,15 +30,27 @@ if __name__ == '__main__':
     ML.model_summary()
     ML.plot_Training()
 
-    #analisa model
-    #akurasi terhadap data training
+    # Analisa model
+    # Akurasi terhadap data training
     pred_train = ML.ModelPredict(load.x_train)
     accuracy_train = accuracy_score(load.y_train, pred_train)
     print("---------------------------------------------")
     print("akurasi terhadap data training = ", accuracy_train)
 
-    #akurasi terhadap data test
+    # Akurasi terhadap data uji
     pred_test = ML.ModelPredict(load.x_test)
     accuracy_test = accuracy_score(load.y_test, pred_test)
     print("akurasi terhadap data test = ", accuracy_test)
     print("---------------------------------------------")
+
+    with tf.io.TFRecordWriter('train_data.tfrecord') as writer:
+        for feature, label in zip(load.x_train, load.y_train):
+            example = serialize_example(feature.numpy(), label.numpy())
+            writer.write(example)
+    print("Data latih disimpan dalam file 'train_data.tfrecord'")
+
+    with tf.io.TFRecordWriter('test_data.tfrecord') as writer:
+        for feature, label in zip(load.x_test, load.y_test):
+            example = serialize_example(feature.numpy(), label.numpy())
+            writer.write(example)
+    print("Data uji disimpan dalam file 'test_data.tfrecord'")
